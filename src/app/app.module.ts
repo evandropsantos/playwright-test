6d18d29cb5312e10921a8bab368656d6e65710ec
Evandro P. Santos <evandropsantos@gmail.com>
2022-06-08 18:02:25 -0300

    stack de testes

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { PrimeiroComponent } from './primeiro/primeiro.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrimeiroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
